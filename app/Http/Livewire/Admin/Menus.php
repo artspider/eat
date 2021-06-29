86ed2d90631f8626631e17bc92aec4c5e05cbdf7
Arturo rodriguez <arturo.itiguala@gmail.com>
2021-06-28 20:21:09 -0500

    recipes migrations prep2

--- app/Http/Livewire/Admin/Menus.php
+++ app/Http/Livewire/Admin/Menus.php
@@ -3,12 +3,28 @@
 namespace App\Http\Livewire\Admin;
 
 use Livewire\Component;
+use App\Models\Recipe;
+use Livewire\WithPagination;
 
 class Menus extends Component
 {
+    use WithPagination;
+
+    protected $queryString = ['search' => ['except' => '']];
+
+    public $search;
+
+    protected $listeners = [
+        'success' => 'newMenu',
+        'deleteMenu' => 'remove'
+    ];
+
     public function render()
     {
-        return view('livewire.admin.menus')
+        return view('livewire.admin.menus',[
+            'recipes' => Recipe::where('name', 'LIKE', "%{$this->search}%")
+                                    ->paginate(5)
+        ])
         ->layout('components.layouts.master');
     }
 }
--- resources/views/livewire/admin/menus.blade.php
+++ resources/views/livewire/admin/menus.blade.php
@@ -1,3 +1,8 @@
 <div>
     {{ Breadcrumbs::render('menus') }}
+
+    <div class="bg-white rounded-md shadow-md p-10 ">
+        <x-utils.subtitle class="mb-4">Lista de recetas</x-utils.subtitle>
+        <hr class=" border-eat-olive-50 mb-6 ">
+    </div>
 </div>
